8b2f5469a8c7557375ef77a2bb2a5f651bfc2aa8
Wojciech Kosla <w.kosla@gmail.com>
2019-12-07 19:14:22 +0100

    Working sections nav btns

---
+++ src/assets/js/script.js
@@ -0,0 +1,40 @@
+const nav = document.querySelector('.nav'),
+  sections = Array.from(document.querySelectorAll('.row')),
+  sectionNavBtns = Array.from(document.querySelectorAll('.sections__btn'));
+
+const scroll = new SmoothScroll('a[href*="#"]', {
+  speed: 500,
+  speedAsDuration: true,
+});
+
+function onWhite(el) {
+  return el.offsetTop + (el.clientHeight / 2) >= sections[1].getBoundingClientRect().top;
+}
+
+function inView(section) {
+  return section.getBoundingClientRect().top < window.innerHeight / 2 && section.getBoundingClientRect().bottom > window.innerHeight / 2;
+}
+
+window.addEventListener('scroll', function() {
+  if (this.scrollY > 0) {
+    nav.classList.add('nav--scroll');
+  } else {
+    nav.classList.remove('nav--scroll');
+  }
+
+  sectionNavBtns.forEach(btn => {
+    if (onWhite(btn)) {
+      btn.classList.add('sections__btn--dark');
+    } else {
+      btn.classList.remove('sections__btn--dark');
+    }
+  });
+
+  sections.forEach((section, i) => {
+    if (inView(section)) {
+      sectionNavBtns[i].classList.add('sections__btn--active');
+    } else {
+      sectionNavBtns[i].classList.remove('sections__btn--active');
+    }
+  })
+});
